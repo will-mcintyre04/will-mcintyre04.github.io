0504a92da97fd102ca8ed781bc593b17b21e4026
will-mcintyre04 <william.d.j.mcintyre@gmail.com>
2023-05-27 01:13:53 -0400

    removed changes to scroll button

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,19 +41,6 @@ function showSlides(n, slideshow) {
   	dots[slideshow.currentSlideIndex-1].className += " active";
 }
 
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 750px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
-	mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
 (function($) {
 
 	var	$window = $(window),
